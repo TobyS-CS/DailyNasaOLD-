98c1afc27d8793fef0c237c70cb0f6dbc125ac4a
TobyS-CS <55004225+TobyS-CS@users.noreply.github.com>
2019-11-26 12:55:40 -0600

    Add files via upload
    after first day

---
+++ .github/workflows/DailyNasa/app/src/main/java/com/example/dailynasa/DailyNasa/app/src/main/java/com/example/dailynasa/RoverPictureView.java
@@ -0,0 +1,14 @@
+package com.example.dailynasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoverPictureView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rover_picture_view);
+    }
+}
